7b21cc61f4a4563c47ac5a578b850f9842eda721
omkar98 <edu.omkar@gmail.com>
2018-06-24 12:13:10 +0530

    fixed date issue

--- csv.php
+++ csv.php
@@ -16,8 +16,11 @@ class csv extends mysqli
         $row = fgetcsv($file);
         while($row)
         {
-            $value  = "'".implode("','",$row)."'"; 
-            $query = "INSERT INTO investments(date,category,sub_category,name,raised,area,round,investors,link) VALUES(". $value .")";
+            date_default_timezone_set('UTC');
+            $timestamp = strtotime($row[0]);
+            $date=date("Y-m-d", $timestamp);
+            $query = "INSERT INTO investments(date,category,sub_category,name,raised,area,round,investors,link) VALUES('$date','$row[1]','$row[2]','$row[3]','$row[4]','$row[5]','$row[6]','$row[7]','$row[8]')";
+
             if($this->query($query))
             {
                $this->state_csv = true;
