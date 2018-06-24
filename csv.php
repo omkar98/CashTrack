7d4a67cea9416325957967b7913df96a5ed6daa6
omkar98 <edu.omkar@gmail.com>
2018-06-24 10:27:03 +0530

    Works well
    The only issue is with the date. It shows 0000-00-00 in the database

--- csv.php
+++ csv.php
@@ -1,6 +1,7 @@
 <?php 
 class csv extends mysqli
 {
+    private $state_csv = false;
     public function __construct()
     {
         parent:: __construct("localhost", "root" , "", "cashtrack");
@@ -15,8 +16,20 @@ class csv extends mysqli
         $row = fgetcsv($file);
         while($row)
         {
-          var_dump($row);
-          $row = fgetcsv($file);
+            $value  = "'".implode("','",$row)."'"; 
+            $query = "INSERT INTO investments(date,category,sub_category,name,raised,area,round,investors,link) VALUES(". $value .")";
+            if($this->query($query))
+            {
+               $this->state_csv = true;
+                echo "Imported File Successfully";
+            }
+            else
+            {
+               $this->state_csv = false;
+                echo $this->error;
+            }
+            
+            $row = fgetcsv($file);
         }
     }
 }
